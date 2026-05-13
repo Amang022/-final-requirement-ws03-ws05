5460dca2d113e418ccedcb45939d9eb2e911f0a2
Mark Christian Mendoza <markchristianmendoza04@gmail.com>
2026-05-13 18:39:59 +0800

    Erase the mitigation

--- database/migrations/001-reset-superadmin-password.sql
+++
@@ -1,6 +0,0 @@
--- Database migration: Reset the seeded Super Admin password.
--- Run this after pulling new branches if the default super admin password needs to be updated.
-
-UPDATE users
-SET password = '$2b$12$ZxvXynbDRT5kQ/MkN5J0h.WLkHNo1UU11uqeJA/BwZSDCHxddXTDK'
-WHERE email = 'superadmin@agri.local';
